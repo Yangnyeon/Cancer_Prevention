4e4d6f8b040ec130f73281be6e5a0772bdcaa8f6
Yangnyeon <tmdgurdla324@naver.com>
2022-07-27 23:19:06 +0900

    공부중..

--- app/src/main/java/com/example/cancer_prevention/MainActivity.kt
+++ app/src/main/java/com/example/cancer_prevention/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.cancer_prevention
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cancer_prevention.Community.Community_Fragment
+import com.example.cancer_prevention.Main.Main_Fragment
 import com.example.cancer_prevention.Retrofit.Cancer_Retrofit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gogo_retrofit.setOnClickListener {
-            startActivity(Intent(this@MainActivity, Cancer_Retrofit::class.java))
+        /*
+
+        gogo_corutine.setOnClickListener {
+            startActivity(Intent(this@MainActivity, corutine::class.java))
+        }
+
+
+         */
+
+        var main = findViewById<BottomNavigationView>(R.id.bottonavi)
+
+
+
+        main.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.first -> {
+                        val MainFragment1 = Main_Fragment()
+                        var haha = BuildConfig.GOOGLE_API_KEY
+                        supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment1).commit()
+                    }
+                    R.id.second -> {
+                        val MainFragment2 = Community_Fragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment2).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.first
         }
 
     }
